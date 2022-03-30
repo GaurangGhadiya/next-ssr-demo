d562dd80b2b0d0268227dc2f089abb9bee46d2e9
gaurang.semicolon <gaurang.semicolon@gmail.com>
2022-03-30 12:22:15 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export async function getServerSideProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log("data", data);
+  return {
+    props: {
+      todos: data,
+    },
+  };
+}
+
+export default function Home({ todos }) {
+  console.log("todos1", todos);
   return (
     <div className={styles.container}>
       <Head>
@@ -11,13 +23,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      {todos?.map((v) => {
+        return (
+          <div key={v?.id}>
+            <p>
+              {v?.id} : {v?.title}
+            </p>
+          </div>
+        );
+      })}
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -51,19 +72,18 @@ export default function Home() {
           </a>
         </div>
       </main>
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
